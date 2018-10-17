2c07798e89e577be3d11e26ab9c775f571d5c156
Amir Bizimana <amirbzmn@gmail.com>
2018-10-16 21:58:39 -0400

    First commit on new portfolio

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var menuBtn = document.getElementsByClassName('menu-btn');
+
+var mobileMenu = document.getElementsByClassName('mobile-menu');
+
+// var clickedBtn = function(){
+//     mobileMenu[0].classList.toggle('active');
+// }
+
+// menuBtn[0].addEventListener('click', clickedBtn);
+console.log(menuBtn[0]);
+
+menuBtn[0].addEventListener('click',function(){
+    mobileMenu[0].classList.toggle('active');
+});
