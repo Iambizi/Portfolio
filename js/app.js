7c2c69b65ec4ba35d5b748e6dd2a99f7f34381b9
Amir Bizimana <amirbzmn@gmail.com>
2018-10-24 20:31:29 -0400

    Initial updates

--- js/app.js
+++ js/app.js
@@ -2,13 +2,13 @@ var menuBtn = document.getElementsByClassName('menu-btn');
 
 var mobileMenu = document.getElementsByClassName('mobile-menu');
 
-// var clickedBtn = function(){
-//     mobileMenu[0].classList.toggle('active');
-// }
+var clickedBtn = function(){
+     mobileMenu[0].classList.toggle('active');
+ }
 
-// menuBtn[0].addEventListener('click', clickedBtn);
+menuBtn[0].addEventListener('click', clickedBtn);
 console.log(menuBtn[0]);
 
-menuBtn[0].addEventListener('click',function(){
+/*menuBtn[0].addEventListener('click',function(){
     mobileMenu[0].classList.toggle('active');
-});
+});*/
