e92b576ef027d0ca9fec8f32a13978975635d0bf
Amir Bizimana <amirbzmn@gmail.com>
2018-11-13 01:20:10 -0500

    interactive changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,30 @@
-var menuBtn = document.getElementsByClassName('menu-btn');
+//Mobile menu button
+const menuBtn = document.getElementsByClassName("menu-btn");
 
-var mobileMenu = document.getElementsByClassName('mobile-menu');
+const mobileMenu = document.getElementsByClassName("mobile-menu");
 
-var clickedBtn = function(){
-     mobileMenu[0].classList.toggle('active');
- }
+const clickedBtn = function() {
+  mobileMenu[0].classList.toggle("active");
+};
 
-menuBtn[0].addEventListener('click', clickedBtn);
+menuBtn[0].addEventListener("click", clickedBtn);
 console.log(menuBtn[0]);
 
 /*menuBtn[0].addEventListener('click',function(){
     mobileMenu[0].classList.toggle('active');
 });*/
+
+//About me text toggle
+$("#about").on("click", function() {
+  $("#about-text").toggle();
+});
+
+//Scroll down
+// $(".page-scroll").on('click', function(){
+
+// });
+
+// Fade-in effect
+$(document).ready(function() {
+  $("body").removeClass("fade-out");
+});
